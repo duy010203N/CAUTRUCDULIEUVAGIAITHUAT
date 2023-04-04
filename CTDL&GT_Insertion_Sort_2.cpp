7da4255f9d68a6c4b7cfa3df01a7aef12161c9c2
duy010203N <125200444+duy010203N@users.noreply.github.com>
2023-04-04 16:14:50 +0700

    Create CTDL&GT_Insertion_Sort_2.cpp

---
+++ CTDL&GT_Insertion_Sort_2.cpp
@@ -0,0 +1,43 @@
+//https://www.geeksforgeeks.org/insertion-sort/
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        /* Di chuyển các phần tử có giá trị lớn hơn giá trị
+        key về sau một vị trí so với vị trí ban đầu của nó */
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+/* Hàm xuất mảng */
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}

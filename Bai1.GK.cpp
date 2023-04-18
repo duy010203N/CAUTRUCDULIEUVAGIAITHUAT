b2bd5b8150fdc16556d2c31f1d186b1ff9928109
duy010203N <125200444+duy010203N@users.noreply.github.com>
2023-04-18 16:02:54 +0700

    Create Bai1.GK.cpp

---
+++ Bai1.GK.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int T(int n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return n * T(n-1);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+    cout << "T(" << n << ") = " << T(n) << endl;
+    return 0;
+}
